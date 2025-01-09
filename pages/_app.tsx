dab2f39cb1213dc567892248797f880009f6b344
Zain🍕 <zainadam.id@gmail.com>
2025-01-09 18:47:09 +0800

    Refactor layout to use `<MainLayout />` and remove global CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
 import type { AppProps } from "next/app";
+import { MainLayout } from "@/components/main-layout";
+import FONT from "@/statics/font";
+import { consoleLog } from "@/utils/console-log";
+
+consoleLog("exec `pages/_app.tsx`");
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	consoleLog("render `<App />`");
+
+	return (
+		<div className={FONT.className}>
+			<MainLayout>
+				<Component {...pageProps} />
+			</MainLayout>
+		</div>
+	);
 }
